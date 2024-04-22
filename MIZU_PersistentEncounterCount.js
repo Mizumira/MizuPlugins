aa836d127de79b63e7bf92e4e5a7e0505e7fc02a
Mizumira <32559721+Mizumira@users.noreply.github.com>
2024-04-23 02:00:28 +1000

    Add files via upload

---
+++ MIZU_PersistentEncounterCount.js
@@ -0,0 +1,26 @@
+//=============================================================================
+// MIZU_PersistentEncounterCount.js
+//=============================================================================
+
+var Mizu = Mizu || {};
+
+/*:
+ * @plugindesc (v0.9) Makes the Encounter Count not reset upon map transitions
+ * @author Mizu
+ *
+ * @help
+ * The count will only reset on hitting 0
+ *
+ *
+ *
+ *=============================================================================
+ *     All my released MV Plugins:   https://github.com/Mizumira/MizuPlugins
+ *=============================================================================
+ */
+
+Mizu.Game_Player_makeEncounterCount = Game_Player.prototype.makeEncounterCount;
+Game_Player.prototype.makeEncounterCount = function() {
+	if (this._encounterCount <= 0) {
+		Mizu.Game_Player_makeEncounterCount.call(this)
+	}
+};
